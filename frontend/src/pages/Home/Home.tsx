2270ae309acc0e79842fb344c49ecd53e1539110
muthamizhselvan R <muthamizhyadav@gmail.com>
2023-04-11 11:14:52 +0530

    asdf

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import "./Home.css";
 import NavBar from "../../components/NavBar";
-
+import banner from "../../assets/banner1.png";
+import "./Home.css";
 function Home() {
   return (
-    <div>
+    <>
       <NavBar />
-      <h1>Home</h1>
-    </div>
+      <div className="home-container">
+        <img src={banner} alt="home-banner" width="100%" />
+        <h1>Distance is no longer a matter</h1>
+        <button className="btn">Join</button>
+      </div>
+    </>
   );
 }
 
